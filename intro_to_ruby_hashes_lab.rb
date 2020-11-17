75a8141b03edd559aea1fd3267ffcd7a171a8328
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-17 19:54:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ end
 # keyword "return." Nice and neat.
 
 def monopoly_with_second_tier
-  {:railroads => {:pieces => 4}}
+  {:railroads => {:pieces => 4, :rent_in_dollars => {}}}
 end
 
 def monopoly_with_third_tier
